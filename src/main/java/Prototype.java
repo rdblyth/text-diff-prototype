1d22a3afaa61c0f607a8c6ed4516a08dceba425a
Daneil Blyth <rdblyth@gmail.com>
2019-04-10 23:45:08 -0400

    Initial commit

---
+++ src/main/java/Prototype.java
@@ -0,0 +1,36 @@
+
+
+import com.github.difflib.algorithm.DiffException;
+import com.github.difflib.text.DiffRow;
+import com.github.difflib.text.DiffRowGenerator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Prototype {
+
+    public static void main(String[] args) throws DiffException {
+        DiffRowGenerator generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .inlineDiffByWord(true)
+                //.oldTag(f -> f ? "<strike>" : "</strike>")
+                .oldTag(f -> f ? "<span style=\"background-color: #FFFF00\">" : "</span>")
+                .build();
+        List<DiffRow> rows = generator.generateDiffRows(
+                Arrays.asList("(1) This is a test sentence.", "(2) This is the second line.", "(3) And here is the finish."),
+                Arrays.asList("This is a test sentence.", "This is the second line.", "And here is the finish."));
+
+        System.out.println("|original|new|");
+        System.out.println("|--------|---|");
+        for (DiffRow row : rows) {
+            System.out.println("|" + row.getOldLine() + "|" + row.getNewLine() + "|");
+        }
+
+        System.out.println("<html>");
+        for (DiffRow row : rows) {
+            System.out.println(row.getOldLine());
+        }
+        System.out.println("</html>");
+
+    }
+}
