5f0ad18a781cbfc8b0e6ff209fa30af01f5841d2
Daneil Blyth <rdblyth@gmail.com>
2019-04-11 00:09:38 -0400

    Added before and after.

--- src/main/java/Prototype.java
+++ src/main/java/Prototype.java
@@ -14,16 +14,23 @@ public class Prototype {
                 .showInlineDiffs(true)
                 .inlineDiffByWord(true)
                 //.oldTag(f -> f ? "<strike>" : "</strike>")
-                .oldTag(f -> f ? "<span style=\"background-color: #FFFF00\">" : "</span>")
+                .oldTag(f -> f ? "<span style=\"background-color: #FFE6E6\"><strike>" : "</strike></span>")
                 .build();
+
+        List<String> originalText = Arrays.asList("(1) This is a test sentence.", "(2) This is the second line.", "(3) And here is the finish.");
+
         List<DiffRow> rows = generator.generateDiffRows(
-                Arrays.asList("(1) This is a test sentence.", "(2) This is the second line.", "(3) And here is the finish."),
+                originalText,
                 Arrays.asList("This is a test sentence.", "This is the second line.", "And here is the finish."));
 
 
         System.out.println("<html>");
+        for (String row: originalText) {
+            System.out.println("<span>" + row + "</span></br>");
+        }
+        System.out.println("</br>");
         for (DiffRow row : rows) {
-            System.out.println(row.getOldLine());
+            System.out.println(row.getOldLine() + "</br>");
         }
         System.out.println("</html>");
 
